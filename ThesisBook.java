3edc00a16710359e4f73e0e380dc44128a252090
FrostyRylai <ramcelmanasis198@gmail.com>
2025-11-23 23:46:44 +0800

    Create ThesisBook.java

---
+++ ThesisBook.java
@@ -0,0 +1,5 @@
+public class ThesisBook extends Material {
+    public ThesisBook(int id, String title, String author, int year, String pub, int copies) {
+        super(id, title, author, year, pub, copies);
+    }
+}
