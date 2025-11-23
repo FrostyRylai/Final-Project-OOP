041d4f454bd439eba760fd81b2b8d44a1a70b774
FrostyRylai <ramcelmanasis198@gmail.com>
2025-11-23 23:45:53 +0800

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,5 @@
+public class Book extends Material {
+    public Book(int id, String title, String author, int year, String pub, int copies) {
+        super(id, title, author, year, pub, copies);
+    }
+}
